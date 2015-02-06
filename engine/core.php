598f56f1b2f4e960f775e399895db183f0928dab
Xunil1337 <cnw1000@ok.de>
2015-02-06 01:56:43 +0100

    Update core.php

--- engine/core.php
+++ engine/core.php
@@ -10,21 +10,19 @@ define('init_engine', true);
 ###################################################################################
 
 //constants
-include_once '/engine/constants.php';
-include_once '/engine/classes/notifications.php';
-include_once '/engine/classes/template.php';
+include_once './engine/constants.php';
 
-require_once '/configuration/config.php';
-
+//General Classes
+require_once './configuration/config.php';
 include_once $config['RootPath'] . '/engine/classes/cache.php';
-include_once $config['RootPath'] . '/admin/engine/classes/multipleError_handler.php';
-include_once $config['RootPath'] . '/admin/engine/classes/sessions.secure.php';
-include_once $config['RootPath'] . '/admin/engine/classes/permissions.php';
-include_once $config['RootPath'] . '/admin/engine/classes/accounts.curuser.php';
-include_once $config['RootPath'] . '/admin/engine/classes/security.php';
-include_once $config['RootPath'] . '/admin/engine/classes/img.manipulation.php';
+include_once $config['RootPath'] . '/engine/classes/template.php';
+include_once $config['RootPath'] . '/engine/classes/multipleError_handler.php';
+include_once $config['RootPath'] . '/engine/classes/sessions.secure.php';
+include_once $config['RootPath'] . '/engine/classes/accounts.curuser.php';
+include_once $config['RootPath'] . '/engine/classes/security.php';
 include_once $config['RootPath'] . '/engine/classes/chmod.calc.php';
-include_once $config['RootPath'] . '/engine/core_modules/coin.activity.php';
+include_once $config['RootPath'] . '/engine/shutdown.php';
+include_once $config['RootPath'] . '/engine/classes/notifications.php';
 
 //storage variables
 include_once $config['RootPath'] . '/engine/storages/boosts.php';
@@ -33,24 +31,26 @@ include_once $config['RootPath'] . '/engine/storages/avatars.php';
 include_once $config['RootPath'] . '/engine/storages/countries.php';
 include_once $config['RootPath'] . '/engine/storages/secret_questions.php';
 include_once $config['RootPath'] . '/engine/storages/voteSites.php';
+include_once $config['RootPath'] . '/engine/storages/pStore_levels.php';
+include_once $config['RootPath'] . '/engine/storages/tp.mapsInfo.php';
+include_once $config['RootPath'] . '/engine/storages/tp.points.php';
 include_once $config['RootPath'] . '/engine/storages/bt.categories.php';
-include_once $config['RootPath'] . '/admin/engine/Item_Classes.php';
 
 //We'll have some alternatives for the sessions
 if (isset($config['SESSION_HANDLER']))
 {
 	if ($config['SESSION_HANDLER'] == 'MCRYPT')
 	{
-		include_once $config['RootPath'] . '/admin/engine/classes/sessions.filesystem.php';
+		include_once $config['RootPath'] . '/engine/classes/sessions.filesystem.php';
 	}
 	else
 	{
-		include_once $config['RootPath'] . '/admin/engine/classes/sessions.none.php';
+		include_once $config['RootPath'] . '/engine/classes/sessions.none.php';
 	}
 }
 else
 {
-	include_once $config['RootPath'] . '/admin/engine/classes/sessions.none.php';
+	include_once $config['RootPath'] . '/engine/classes/sessions.none.php';
 }
 
 ###################################################################################
@@ -61,6 +61,7 @@ class CORE
 {
 	private $config;
 	private $db = false;
+	private $auth_db = false;
 	private $Modules;
 	private $LoadedModules;
 	private $RealmConnections;
@@ -80,56 +81,79 @@ class CORE
 	{
 		global $PDO_config;
 		
-		try 
+		if (!$this->db)
 		{
-			//Construct PDO
-			$obj = new PDO('mysql:dbname='.$this->config['DatabaseName'].'; host='.$this->config['DatabaseHost'].';', $this->config['DatabaseUser'], $this->config['DatabasePass'], NULL);
+			try 
+			{
+				//Construct PDO
+				$obj = new PDO('mysql:dbname='.$this->config['DatabaseName'].'; host='.$this->config['DatabaseHost'].';', $this->config['DatabaseUser'], $this->config['DatabasePass'], NULL);
+				
+				//set error handler exception
+				$obj->setAttribute(PDO::ATTR_ERRMODE, $PDO_config['errorHandler']);
+				
+				//set default fetch method
+				$obj->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, $PDO_config['fetch']);
+				
+				//set encoding
+				$obj->query("SET NAMES '".$this->config['DatabaseEncoding']."'");
+			}
+			catch (PDOException $e)
+			{
+				echo '<strong>Database Connection failed:</strong> Unable to connect to the Web Database.<br><br>';
+				
+				if ($this->config['DEBUG'])
+				{
+					echo $e->getMessage();
+				}
+				die;
+			}
 			
-			//set error handler exception
-			$obj->setAttribute(PDO::ATTR_ERRMODE, $PDO_config['errorHandler']);
+			//save the database connection because we wont be able to open new one
+			$this->db = $obj;
 			
-			//set default fetch method
-			$obj->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, $PDO_config['fetch']);
-			
-			//set encoding
-			$obj->query("SET NAMES '".$this->config['DatabaseEncoding']."'");
-		}
-		catch (PDOException $e)
-		{
-			echo '<strong>Database Connection failed:</strong> Unable to connect to the Web Database.';
-			die;
+			//unset the config variables
+			unset($this->config['DatabaseName'], $this->config['DatabaseHost'], $this->config['DatabaseUser'], $this->config['DatabasePass']);
 		}
 		
-		$this->db = $obj;
-		
-  	  return $obj;
+  	 	return $this->db;
 	}
 	
 	public function AuthDatabaseConnection()
 	{
 		global $auth_config, $PDO_config;
 		
-		try 
+		if (!$this->auth_db)
 		{
-			//Construct PDO
-			$obj = new PDO('mysql:dbname='.$auth_config['DatabaseName'].'; host='.$auth_config['DatabaseHost'].';', $auth_config['DatabaseUser'], $auth_config['DatabasePass'], NULL);
+			try 
+			{
+				//Construct PDO
+				$obj = new PDO('mysql:dbname='.$auth_config['DatabaseName'].'; host='.$auth_config['DatabaseHost'].';', $auth_config['DatabaseUser'], $auth_config['DatabasePass'], NULL);
+				
+				//set error handler exception
+				$obj->setAttribute(PDO::ATTR_ERRMODE, $PDO_config['errorHandler']);
+				
+				//set default fetch method
+				$obj->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, $PDO_config['fetch']);
+				
+				//set encoding
+				$obj->query("SET NAMES '".$auth_config['DatabaseEncoding']."'");
+			}
+			catch (PDOException $e)
+			{
+				echo '<strong>Database Connection failed:</strong> Unable to connect to the Authentication Database.<br><br>';
+				
+				if ($this->config['DEBUG'])
+				{
+					echo $e->getMessage();
+				}
+				die;
+			}
 			
-			//set error handler exception
-			$obj->setAttribute(PDO::ATTR_ERRMODE, $PDO_config['errorHandler']);
-			
-			//set default fetch method
-			$obj->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, $PDO_config['fetch']);
-			
-			//set encoding
-			$obj->query("SET NAMES '".$auth_config['DatabaseEncoding']."'");
-		}
-		catch (PDOException $e)
-		{
-			echo '<strong>Database Connection failed:</strong> Unable to connect to the Authentication Database.';
-			die;
+			//save the database connection because we wont be able to open new one
+			$this->auth_db = $obj;
 		}
 				
-  	  return $obj;
+  	  return $this->auth_db;
 	}
 
 	public function RealmDatabaseConnection($id)
@@ -165,6 +189,9 @@ class CORE
 			{
 				//store the newly made connection and return it
 				$this->RealmConnections[$id] = $obj;
+				//the connection is made, unset the config
+				unset($config, $GLOBALS['realms_config'][$id]['Database']);
+				
 				return $obj;
 			}
 			else
@@ -229,6 +256,8 @@ class CORE
 	
 	public function load_CoreModule($name)
 	{
+		global $config, $CORE;
+		
 		if ($this->isRegistred_Module($name, 'CORE'))
 		{
 			if (file_exists($this->Modules['CORE'][$name]))
@@ -309,53 +338,31 @@ class CORE
 		}
 	}
 	
-	public function LoadHeader()
+	//Check if the visitor is a bot
+	public function isCrawler($userAgent)
 	{
-		global $CORE, $CURUSER, $DB, $pageName;
+		$crawlers = 'Google|msnbot|Rambler|Yahoo|AbachoBOT|accoona|' .
+		'AcioRobot|ASPSeek|CocoCrawler|Dumbot|FAST-WebCrawler|' .
+		'GeonaBot|Gigabot|Lycos|MSRBOT|Scooter|AltaVista|IDBot|eStyle|Scrubby';
 		
-		$HeaderTitle = $this->config['SiteName'];
-		$config = $this->config;
+		$isCrawler = (preg_match("/$crawlers/", $userAgent) > 0);
 		
-		define('init_template', true);
-	
-		require_once $this->config['RootPath'] . '/admin/template/header.php';
+		return $isCrawler;
 	}
 	
-	public function LoadFooter()
+	public function cookiesEnabled()
 	{
-		require_once $this->config['RootPath'] . '/admin/template/footer.php';
-	}
-	
-	public function ErrorBox($text)
-	{
-		echo '<!-- The content -->
-			<section id="content">
-				<div class="tab" id="maintab">
-					<h2>An error occured!</h2>';
-					
-					echo '<p>' . $text . '</p>';
-					
-		echo	'</div>';
+		//Try setting a cookie	
+		setcookie('__Test', 1, time()+60*5);
 		
-		//We must load the footer
-		$this->LoadFooter();
-		
-		die;
-	}
-	
-	public function CheckPermissionsExecute($index)
-	{
-		global $ERRORS, $CURUSER;
-		
-		//check for permissions
-		$ERRORS->NewInstance('permissions');
-		
-		if (!$CURUSER->getPermissions()->isAllowed($index))
+		if(!isset($_COOKIE))
 		{
-			$ERRORS->Add('You do not have the required permissions.');
+			return false;
 		}
-		
-		$ERRORS->Check('/index.php?page=home');
+		else
+		{
+			return true;
+		}
 	}
 	
 	//Using that to store the page URL before login is requested
@@ -369,6 +376,31 @@ class CORE
   	  return $url;
 	}
 	
+	public function ValidateURLBeforeLogin($url)
+	{
+		//if we are on localhost return true no matter
+		if ($_SERVER['HTTP_HOST'] == 'localhost')
+		{
+			return true;
+		}
+		
+		//check if it is valid URL
+		if(!preg_match("/^[a-zA-Z]+[:\/\/]+[A-Za-z0-9\-_]+\\.+[A-Za-z0-9\.\/%&=\?\-_]+$/i", $url))
+		{
+			return false;
+		}
+		
+		//check if the redirection is to our host dont allow outside of the host
+		$URL_Host = parse_url($url, PHP_URL_HOST);
+		
+		if ($URL_Host != $_SERVER['HTTP_HOST'])
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
 	public function loggedInOrReturn()
 	{
     	global $CURUSER;
@@ -376,7 +408,7 @@ class CORE
     	if (!$CURUSER->isOnline())
 		{   
 	    	$_SESSION['url_bl'] = $this->getPageURL();
-        	header("Location: ".$this->config['BaseURL']."/admin/login.php");
+        	header("Location: ".$this->config['BaseURL']."/index.php?page=login");
         	die;
     	}
 	}
@@ -436,9 +468,39 @@ class CORE
 		}
 	}
 	
+	public function getWeekStartEnd($date = false)
+	{
+		if (!$date)
+			$date = $this->getTime();
+			
+       	$ts = strtotime($date);
+		
+		//check if today is monday
+		if (date('w', $ts) == 1)
+		{
+			$start = strtotime('today', $ts);
+		}
+		else
+		{
+			$start = strtotime('this week last monday', $ts);
+		}
+		//Check if today is sunday
+		if (date('w', $ts) == 0)
+		{
+			$end = strtotime('today', $ts);
+		}
+		else
+		{
+			$end = strtotime('this week next sunday', $ts);
+		}
+		
+		//Add 23 hours and 59 minutes to the END date
+		return array(date('Y-m-d H:i', $start), date('Y-m-d H:i', $end + 60*60*23 + 60*59));
+	}
+	
 	public function convertDataTime($timestamp)
 	{
-	  global $CORE;
+	  	global $CORE;
 	  		
 		//setup DateTime Object with the timestamp from the parameter
 		$dateTime = $CORE->getTime(true, $timestamp);
@@ -455,7 +517,7 @@ class CORE
 		{
 			if ($hours > 0)
 			{
-				$return_time = $hours . ' hours, ' . $minutes . ' minutes ago';
+				$return_time = $hours . ' hours  ago';
 			}
 			else if ($minutes > 0)
 			{
@@ -499,7 +561,47 @@ class CORE
 							
       return $return_date_time;
 	}
-
+	
+	public function singleMeasureTimeLeft($timestamp)
+	{
+	  	global $CORE;
+	  		
+		//setup DateTime Object with the timestamp from the parameter
+		$dateTime = $CORE->getTime(true);
+		$dateTime->setTimestamp($timestamp);
+		//setup new DateTime Object from the time ATM
+		$now = $CORE->getTime(true);
+		
+		//get time diference
+		$days = $dateTime->diff($now)->d;
+		$hours = $dateTime->diff($now)->h;
+		$minutes = $dateTime->diff($now)->i;
+		$seconds = $dateTime->diff($now)->s;
+				
+		if ($days >= 30)
+		{
+			return '<b>1</b> month';
+		}
+		else if ($days > 0)
+		{
+			return '<b>' . $days . '</b> day' . ($days == 1 ? '' : 's');
+		}
+		else if ($hours > 0)
+		{
+			return '<b>' . $hours . '</b> hour' . ($hours == 1 ? '' : 's');
+		}
+		else if ($minutes > 0)
+		{
+			return '<b>' . $minutes . '</b> minute' . ($minutes == 1 ? '' : 's');
+		}
+		else if ($seconds > 0)
+		{
+			return '<b>' . $seconds . '</b> second' . ($seconds == 1 ? '' : 's');
+		}
+		
+		return false;
+	}
+	
 	public function convertCooldown($timestamp)
 	{
 	  global $CORE;
@@ -531,6 +633,12 @@ class CORE
 		return $count;
 	}
 	
+	public function convertBytes($size)
+	{
+		$unit=array('b','kb','mb','gb','tb','pb');
+		return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
+	}
+	
 	public function ExecuteSoapCommand($command, $realmid)
 	{
 		global $realms_config;
@@ -556,6 +664,63 @@ class CORE
 	    }
 		 
 	    return array('sent' => true, 'message' => $result);
+	}
+	
+	public function getRemotePage($parameters)
+	{
+		if (!isset($parameters['page']))
+			$parameters['page'] = '/';
+		
+		if (!isset($parameters['timeout']))
+			$parameters['timeout'] = 0.5;
+		
+		//Return array
+		$return = array();
+		
+		$fp = fsockopen($parameters['host'], $parameters['port'], $errno, $errdesc, $parameters['timeout']);
+		if (!$fp)
+		{
+			$return['error'] = 'Couldn\'t connect to '.$parameters['host'].':\nError: '.$errno.'\nDesc: '.$errdesc.'\n';
+		}
+			
+		$request = "GET ".$parameters['page']." HTTP/1.0\r\n";
+		$request .= "Host: ".$parameters['host']."\r\n";
+		$request .= "Referer: http://www.warcry-wow.com\r\n";
+		$request .= "User-Agent: Warcry WoW PHP Client\r\n\r\n";
+		
+		//Prepare the page
+		$return['headers'] = '';
+		$return['body'] = '';
+		
+		fputs($fp, $request);
+
+		// put the header in variable
+		do // loop until the end of the header
+		{
+			$return['headers'] .= fgets($fp, 128);
+
+		} while(strpos($return['headers'], "\r\n\r\n") === false);
+
+		// now put the body in variable
+		while (!feof($fp))
+		{
+			$return['body'] .= fgets ($fp, 128);
+		}
+	
+		fclose($fp);
+		
+		return $return;
+	}
+	
+	public function setCookie($key, $value, $expire, $path = '/', $domain = '')
+	{
+		setcookie($key . '_wcw', $value, $expire, $path, $domain, isset($_SERVER["HTTPS"]), true);
+	}
+	
+	public function removeCookie($key, $path = '/', $domain = '')
+	{
+		if (isset($_COOKIE[$key . '_wcw']))
+			setcookie($key . '_wcw', "", time()-3600, $path, $domain);
 	}
 	
 	public function getItemQualityString($id)
@@ -594,48 +759,43 @@ class CORE
 		return false;
 	}
 	
-	public function RecursiveRemoveDirectory($directory, $empty = false)
+	public function getClassString($id)
 	{
-		if (substr($directory,-1) == '/')
+		switch($id)
 		{
-			$directory = substr($directory,0,-1);
+			case 1:
+				return 'Warrior';
+				break;
+			case 2:
+				return 'Paladin';
+				break;
+			case 3:
+				return 'Hunter';
+				break;
+			case 4:
+				return 'Rogue';
+				break;
+			case 5:
+				return 'Priest';
+				break;
+			case 6:
+				return 'Death Knight';
+				break;
+			case 7:
+				return 'Shaman';
+				break;
+			case 8:
+				return 'Mage';
+				break;
+			case 9:
+				return 'Warlock';
+				break;
+			case 11:
+				return 'Druid';
+				break;
 		}
 		
-		if (!file_exists($directory) || !is_dir($directory))
-		{
-			return false;
-		}
-		elseif (is_readable($directory))
-		{
-			$handle = opendir($directory);
-			
-			while (false !== ($item = readdir($handle)))
-			{
-				if ($item != '.' && $item != '..')
-				{
-					$path = $directory.'/'.$item;
-					if (is_dir($path)) 
-					{
-						$this->RecursiveRemoveDirectory($path);
-					}
-					else
-					{
-						unlink($path);
-					}
-				}
-			}
-			closedir($handle);
-			
-			if ($empty == false)
-			{
-				if (!rmdir($directory))
-				{
-					return false;
-				}
-			}
-		}
-		
-		return true;
+		return false;
 	}
 	
 	public function ChmodWritable($path)
@@ -672,8 +832,8 @@ class CORE
 		{
 			$chmod = new ChmodCalc();
 			$chmod->setOwnermodes(true,true,true);
-			$chmod->setGroupmodes(true,false,true);
-			$chmod->setPublicmodes(true,false,true);
+			$chmod->setGroupmodes(true,false,false);
+			$chmod->setPublicmodes(true,false,false);
 			$ChmodPermissions = $chmod->getMode();
 			//chmod it
 			chmod($path, $ChmodPermissions);
@@ -692,11 +852,27 @@ class CORE
 		return false;
 	}
 	
+	public function hasFlag($flags, $flag)
+	{
+        return ($flags & $flag) != 0;
+    }
+
+    public function setFlag(&$flags, $flag) 
+	{
+		 $flags |= $flag;
+	}
+	
+	public function removeFlag(&$flags, $flag)
+	{
+		$flags &= ~$flag;
+	}
+	
 	public function __destruct()
 	{
 		//kill and unset the website DB
-		$this->db = NULL;
 		unset($this->db);
+		//kill and unset the auth DB
+		unset($this->auth_db);
 		//unset the modules variable
 		unset($this->Modules);
 		//close the stored realm connections
